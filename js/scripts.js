4fc79a8c1348f7a1d686871a96758dddde4c1539
Malin-UIUX <malinlinahausmann@gmail.com>
2026-02-26 14:46:41 +0100

    Mobile accessibility
    Implemented mobile menue with toggle function

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,21 @@
 // This will print a message in the browser console
 console.log("Hey, welcome to my portfolio site!")
+
+
+function menuToggle() { 
+    var nav = document.getElementById("myNavtoggle"); 
+    if (nav.className === "navtoggle") { 
+        nav.className += " responsive"; } 
+    else { nav.className = "navtoggle"; 
+    } 
+}
+
+$(document).ready(function(){ 
+    $("a[href^='#']").on('click', function(e) { 
+        e.preventDefault(); 
+        var target = this.hash; 
+        $('html, body').animate({ 
+            scrollTop: $(target).offset().top 
+        }, 600); 
+    }); 
+});
